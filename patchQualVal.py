5852fa2c9b48be8e9cfa49aae634fa5359897122
eliasekstedt <ekstedt.mbt@gmail.com>
2026-01-30 14:35:17 +0100

    working on quality filter while desktop crashes, not yet burning

--- patchQualVal.py
+++ patchQualVal.py
@@ -1,38 +1,73 @@
 
 from pbo_config import cfg
 from pathlib import Path
+from PIL import Image
+from tqdm import tqdm
 import openslide
 import h5py
 import numpy as np
-from PIL import Image
-from tqdm import tqdm
+import cv2
+
+from skimage.filters import threshold_otsu
+import matplotlib.pyplot as plt
+
+def gshow(tensor):
+    plt.imshow(tensor, cmap='grey')
+    plt.show()
 
 class Validator:
     def __init__(self, dpath_mrxs, dpath_coords, dpath_samples):
-        for fpath_mrxs in tqdm(dpath_mrxs.iterdir()):
+        for fpath_mrxs in tqdm(dpath_mrxs.iterdir(), total=len(list(dpath_mrxs.iterdir()))):
             if not fpath_mrxs.is_file():
                 continue
 
             fpath_coords = dpath_coords / fpath_mrxs.name.replace('.mrxs', '.h5')
             fpath_sample = dpath_samples / fpath_mrxs.name.replace('.mrxs', '.png').lstrip('patient_')
+            """
             if fpath_sample.is_file():
                 with open(dpath_samples / 'skip.txt', 'a') as file:
                     file.write(f"skipping {fpath_mrxs}")
                 continue
+            """
             
             wsi = openslide.open_slide(fpath_mrxs)
             coords, patch_size, lvl = self.get_sample_meta(fpath_coords)
             sample = self.assemble_sample(wsi, coords, patch_size, lvl)
-            Image.fromarray(sample).save(fpath_sample)
+            #Image.fromarray(sample).save(fpath_sample)
             
     def assemble_sample(self, wsi, coords, size, lvl):
-        assem, row = None, None
-        for pos in coords:
+        def process_patch(patch):
+            patch = cv2.cvtColor(patch[:, :, ::-1], cv2.COLOR_BGR2GRAY)
+            patch = cv2.GaussianBlur(patch, (15, 15), sigmaX=0)
+            binary = np.array(patch < 245).astype(np.int16) * 255
+
+            kernel = np.ones((3, 3), np.uint8)
+            morphed_e = cv2.erode(binary, kernel, iterations=3)
+            morphed_d = cv2.dilate(morphed_e, kernel, iterations=1)
+            print(np.mean(np.where(morphed_d > 0, 1, 0)))
+            gshow(np.concatenate([
+                np.concatenate([patch, binary], axis=1),
+                np.concatenate([morphed_d, morphed_e], axis=1),
+            ], axis=0))
+            """
+            """
+            
+
+        def read_patch(wsi, pos, lvl, size):
             patch = np.array(wsi.read_region(
                 location=pos,
                 level=lvl,
                 size=(size, size)
             ).convert('RGB'))
+            return patch
+        
+        assem, row = None, None
+        for pos in coords:
+            patch = read_patch(wsi, pos, lvl, size)
+
+            if True:
+                process_patch(patch)
+
             if row is None:
                 row = patch
             else:
@@ -43,23 +78,20 @@ class Validator:
                     else:
                         assem = np.concatenate([assem, row], axis=0)
                     row = None
+        plt.imshow(assem)
+        plt.show()
+        raise SystemExit
         return assem
 
     def get_sample_meta(self, fpath_coords, n=25):
         coords_for_sample_patches = []
         with h5py.File(fpath_coords, "r") as f:
             avail_coords = f['coords']
-            #print(avail_coords.shape)
-            #raise SystemExit
             [coords_for_sample_patches.append(avail_coords[i])
              for i in np.random.choice(range(avail_coords.shape[0]), n)]
             patch_size = f['coords'].attrs['patch_size']
             lvl = f['coords'].attrs['patch_level']
         return coords_for_sample_patches, patch_size, lvl
-
-
-
-
 
 dpath_mrxsRoot = Path(cfg.dpath_mrxsRoot)
 dpath_coords = Path(cfg.dpath_patchset)
@@ -70,29 +102,3 @@ Validator(
     dpath_samples=cfg.dpath_patch2encode_samples,
 )
 
-
-
-
-
-
-
-"""
-print(
-    "\nmrxs: {}\ncoord: {}\nsample: {}".format(
-        fpath_mrxs, fpath_coord, fpath_sample
-))
-"""
-
-"""
-for name in coord_names:
-    assert (dpath_coords / name).is_file()
-[print(i, wsin, cn) for i, (wsin, cn) in enumerate(zip(wsi_names, coord_names))]
-"""
-"""
-fpath_coords = [
-
-]
-for fpath in fpaths_wsi:
-    wsi_obj = WholeSlideImage(fpath)
-    self.gen_wsi_sample(wsi_obj)
-"""
